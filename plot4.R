dd3e401bf6272d2e35e35cf002a6d2584032e600
rcourse <martinum@gmail.com>
2015-04-10 15:26:39 +0200

    plot4 code

--- plot4.R
+++ plot4.R
@@ -37,7 +37,7 @@ plot4 <- function() {
   plot(set$timestamp, set$Sub_metering_1, type="l", xlab=NA, ylab="Energy sub metering")
   lines(set$timestamp, set$Sub_metering_2, col="red")
   lines(set$timestamp, set$Sub_metering_3, col="blue")
-  legend("topright", lty=c(1,1,1), col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  legend("topright", lty=c(1,1,1), col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n")
   
   # plot 4 construction
   plot(set$timestamp, set$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
